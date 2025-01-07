dd880cda6344f34e91c32d3cad5f4cf719c0137e
jovani-x <jovani-x@users.noreply.github.com>
2025-01-07 09:59:05 +0100

    Fix ProtectedRoute

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../auth";
 
 const ProtectedRoute = ({ children }: { children?: React.ReactNode }) => {
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }: { children?: React.ReactNode }) => {
     return <Navigate to={"/login"} replace />;
   }
 
-  return <>{children}</>;
+  return <>{children ?? <Outlet />}</>;
 };
 
 export default ProtectedRoute;
