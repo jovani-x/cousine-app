0be2d83caa4494e1ade46ac41eb810f0af9bdc62
jovani-x <jovani-x@users.noreply.github.com>
2025-01-17 21:41:06 +0100

    Update theme - override MuiLink styles

--- src/theme.ts
+++ src/theme.ts
@@ -1,16 +1,58 @@
 import { amber } from "@mui/material/colors";
-import { createTheme } from "@mui/material/styles";
+import {
+  createTheme,
+  Palette,
+  PaletteColor,
+  Theme,
+} from "@mui/material/styles";
+
+const generateLinkColors = (theme: Theme) => {
+  const colorKeys = Object.keys(theme.palette).filter((key) => {
+    const color = theme.palette[key as keyof Palette];
+    return !!color && typeof color === "object" && "main" in color;
+  });
+
+  return colorKeys.map((color) => {
+    const colorValue = theme.palette[color as keyof Palette] as PaletteColor;
+
+    return {
+      props: { color },
+      style: {
+        "--link-color": colorValue[theme.palette.mode],
+        "--link-hover-color": colorValue.main,
+      },
+    };
+  });
+};
 
 const theme = createTheme({
   cssVariables: true,
   palette: {
     mode: "dark",
     primary: {
-      main: amber["500"],
+      main: amber[500],
       dark: amber[600],
     },
     secondary: {
       main: amber["A400"],
+    },
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: "var(--link-color)",
+          textUnderlineOffset: "0.25em",
+          textDecorationColor: "unset",
+          textDecorationThickness: "1px",
+
+          "&:hover": {
+            color: "var(--link-hover-color)",
+          },
+
+          variants: generateLinkColors(theme),
+        }),
+      },
     },
   },
 });
