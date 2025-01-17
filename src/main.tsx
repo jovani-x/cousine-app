f8f6bb8455dc05aefd0a3aa66e62f23eced7355d
jovani-x <jovani-x@users.noreply.github.com>
2025-01-17 21:51:04 +0100

    Add CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -9,6 +10,7 @@ import theme from "./theme";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider
         router={router}
         // v6 -> v7 https://reactrouter.com/upgrading/v6
