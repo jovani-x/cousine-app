22497eb23324044266c5e8479eb05200569f3561
jovani-x <jovani-x@users.noreply.github.com>
2025-01-21 17:17:36 +0100

    Refactor Trivia component to improve readability and extends properties

--- src/components/Trivia/index.tsx
+++ src/components/Trivia/index.tsx
@@ -1,37 +1,44 @@
-import { Paper, Skeleton, Typography, useTheme } from "@mui/material";
+import {
+  Paper,
+  PaperOwnProps,
+  Skeleton,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { useGetTrivia } from "../../hooks/useGetTrivia";
 import { getErrorMessage } from "../../utils/helpers";
 import ErrorMessage from "../ErrorMessage";
 
-export const Trivia = () => {
+export const Trivia = ({ paperProps }: { paperProps?: PaperOwnProps }) => {
   const theme = useTheme();
+  const color = theme.palette.primary[theme.palette.mode];
   const { trivia, isError, error, isPending } = useGetTrivia();
+  const { sx, ...paperPropsRest } = paperProps || {};
+
+  const renderedError = <ErrorMessage error={getErrorMessage(error)} />;
+
+  const renderedContent =
+    !isPending && !trivia?.text ? (
+      <ErrorMessage error="Sorry. Something went wrong." />
+    ) : (
+      <Typography>
+        {isPending ? (
+          <>
+            <Skeleton />
+            <Skeleton />
+          </>
+        ) : (
+          trivia!.text
+        )}
+      </Typography>
+    );
 
   return (
-    <Paper sx={{ p: 2, aspectRatio: "unset" }}>
-      <Typography
-        component="h2"
-        variant="h5"
-        sx={{ mb: 1, color: theme.palette.primary[theme.palette.mode] }}
-      >
+    <Paper {...paperPropsRest} sx={{ p: 2, aspectRatio: "unset", ...sx }}>
+      <Typography component="h2" variant="h5" sx={{ mb: 1, color }}>
         Cuisine Trivia
       </Typography>
-      {isError ? (
-        <ErrorMessage error={getErrorMessage(error)} />
-      ) : !isPending && !trivia?.text ? (
-        <ErrorMessage error="Sorry. Something went wrong." />
-      ) : (
-        <Typography component="p">
-          {isPending ? (
-            <>
-              <Skeleton />
-              <Skeleton />
-            </>
-          ) : (
-            trivia!.text
-          )}
-        </Typography>
-      )}
+      {isError ? renderedError : renderedContent}
     </Paper>
   );
 };
