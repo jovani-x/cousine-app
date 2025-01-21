a9af44dc74188f1173dfb441770926b278963385
jovani-x <jovani-x@users.noreply.github.com>
2025-01-21 18:05:51 +0100

    Update Home view: replace placeholder with components

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,30 @@
+import Grid from "@mui/material/Grid2";
+import { DishStats } from "../../components/DishStats";
+import { HomeWelcome } from "../../components/HomeWelcome";
+import { Trivia } from "../../components/Trivia";
+import { TryNewRecipe } from "../../components/TryNewRecipe";
+
 const HomePage = () => {
-  return "Home Page";
+  const size = { xs: 12, md: 6, lg: 4 };
+
+  return (
+    <>
+      <Grid container spacing={3} sx={{ my: 4 }}>
+        <Grid size={12}>
+          <HomeWelcome />
+        </Grid>
+        <Grid size={size}>
+          <DishStats />
+        </Grid>
+        <Grid size={size}>
+          <TryNewRecipe />
+        </Grid>
+        <Grid size={size}>
+          <Trivia />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default HomePage;
