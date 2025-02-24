ad034437b29529d4005704b297b9465f92d5f816
jovani-x <jovani-x@users.noreply.github.com>
2025-02-24 16:48:15 +0100

    Configure deployment: add VITE_BASE, setup gh-pages, update scripts in package.json

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -69,6 +69,7 @@ const router = createBrowserRouter(
       v7_partialHydration: true,
       v7_skipActionErrorRevalidation: true,
     },
+    basename: import.meta.env.VITE_BASE || "/",
   }
 );
 
